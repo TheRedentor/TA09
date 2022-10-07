f00739a18e3f15ca2342b2e8142a0424f0534fa8
JuanManuelDS <juanmadesenzi@gmail.com>
2022-10-07 09:40:19 +0200

    persona un poco más completa

--- src/ejercicio5/Persona.java
+++ src/ejercicio5/Persona.java
@@ -1,5 +1,15 @@
 package ejercicio5;
 
 public class Persona {
-
+	protected String nombre, sexo;
+	protected int edad, asistencia;
+	
+	
+	public Persona(String nombre, int edad, String sexo, int asistencia) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.sexo = sexo;
+		this.asistencia = asistencia;
+	}
+	
 }
