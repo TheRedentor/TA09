7c271609b2c3d34135d1acb7477cf5d7ca461eda
JuanManuelDS <juanmadesenzi@gmail.com>
2022-10-07 10:29:47 +0200

    atributos persona modificado

--- src/ejercicio5/Persona.java
+++ src/ejercicio5/Persona.java
@@ -3,6 +3,7 @@ package ejercicio5;
 public abstract class Persona {
 	protected String nombre, sexo;
 	protected int edad, asistencia;
+	protected boolean asistio;
 	
 	
 	public Persona() {
@@ -10,13 +11,15 @@ public abstract class Persona {
 		this.edad = 0;
 		this.sexo ="";
 		this.asistencia=0;
+		this.asistio=false;
 	}
 	
-	public Persona(String nombre, int edad, String sexo, int asistencia) {
+	public Persona(String nombre, int edad, String sexo, int asistencia, boolean asistio) {
 		this.nombre = nombre;
 		this.edad = edad;
 		this.sexo = sexo;
 		this.asistencia = asistencia;
-	}                                 
-	                                      
+		this.asistio=asistio;
+	}
+	
 }
