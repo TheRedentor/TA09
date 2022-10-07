71f03af75e57158b535114bda76031e2b4409a37
JuanManuelDS <juanmadesenzi@gmail.com>
2022-10-07 12:30:21 +0200

    hayClases

--- src/ejercicio5/Ejercicio5App.java
+++ src/ejercicio5/Ejercicio5App.java
@@ -3,10 +3,10 @@ package ejercicio5;
 public class Ejercicio5App {
 
 	public static void main(String[] args) {
-		Alumno al = new Alumno();
-		
-		System.out.println(al.toString());
-
+		Alumno alumno1 = new Alumno();
+		alumno1.toString();
 	}
+	
+	
 
 }
