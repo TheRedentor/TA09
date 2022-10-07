0d7a288260ccd1abe9256e8f71fa689ae1f0cfd6
Jaume Curto Castellví <jaumecurtojob@gmail.com>
2022-10-07 13:03:54 +0200

    Cambios de return en Metodos

--- src/ejercicio5/Ejercicio5App.java
+++ src/ejercicio5/Ejercicio5App.java
@@ -2,11 +2,8 @@ package ejercicio5;
 
 public class Ejercicio5App {
 
-	public static void main(String[] args) {
-	
-		Profesor profesorMatematica = new Profesor("Jose", 35, "hombre", "matematicas");
-		Profesor profesorFilosofia = new Profesor("María", 45, "mujer", "filosofia");
-		Profesor profesorFisica = new Profesor("Pepe", 60, "hombre", "fisica");
+	public static void main(String[] args) { 
+		
 		
 	}
 	
--- src/ejercicio5/Metodos.java
+++ src/ejercicio5/Metodos.java
@@ -2,31 +2,43 @@ package ejercicio5;
 
 import ejercicio5.Aula;
 import ejercicio5.Materia;
+import ejercicio5.Profesor;
+import ejercicio5.Alumno;
 
 public class Metodos {
 
-	public void iniciarAulas() {
-		 Aula aulaFilosofia = new Aula(1, 5, Materia.filosofia.toString());
-		 Aula aulaFisica = new Aula(2, 8, Materia.fisica.toString());
-		 Aula aulaMatematicas = new Aula(3, 10, Materia.matematicas.toString());
+	public Aula[] iniciarAulas() {
+		Aula[] aulas = new Aula[3];
+		aulas[0] = new Aula(1, 5, Materia.filosofia.toString());
+		aulas[1] = new Aula(2, 8, Materia.fisica.toString());
+		aulas[2] = new Aula(3, 10, Materia.matematicas.toString());
+		
+		return aulas;
 	 }
 	
-	public void iniciarProfesore() {
-		Profesor profesorMatematica = new Profesor("Jose", 35, "hombre", Materia.matematicas.toString());
-	    Profesor profesorFilosofia = new Profesor("Mar�a", 45, "mujer", Materia.filosofia.toString());
-	    Profesor profesorFisica = new Profesor("Pepe", 60, "hombre", Materia.fisica.toString());
+	public Profesor[] iniciarProfesore() {
+		Profesor[] profesores = new Profesor[3];
+		profesores[0] = new Profesor("Jose", 35, "hombre", Materia.matematicas.toString());
+		profesores[1] = new Profesor("Mar�a", 45, "mujer", Materia.filosofia.toString());
+		profesores[2] = new Profesor("Pepe", 60, "hombre", Materia.fisica.toString());
+		
+		return profesores;
 	}
 	
-	public void iniciarAlumnos() {
-		Alumno alumno1 = new Alumno("Pepe", 13, "hombre", 7);
-		Alumno alumno2 = new Alumno("Maria", 12, "mujer", 9);
-		Alumno alumno3 = new Alumno("Luis", 12, "hombre", 5);
-		Alumno alumno4 = new Alumno("Oscar", 15, "hombre", 6);
-		Alumno alumno5 = new Alumno("Ana", 14, "mujer", 7);
-		Alumno alumno6 = new Alumno("Jaume", 13, "hombre", 8);
-		Alumno alumno7 = new Alumno("Juan", 12, "hombre", 8);
-		Alumno alumno8 = new Alumno("Joel", 14, "hombre", 8);
-		Alumno alumno9 = new Alumno("Rosa", 15, "mujer", 7);
-		Alumno alumno10 = new Alumno("Marta", 13, "mujer", 8);
+	public Alumno[] iniciarAlumnos() {
+		Alumno[] alumnos = new Alumno[10];
+		
+		alumnos[0] = new Alumno("Pepe", 13, "hombre", 7);
+		alumnos[1] = new Alumno("Maria", 12, "mujer", 9);
+		alumnos[2] = new Alumno("Luis", 12, "hombre", 5);
+		alumnos[3] = new Alumno("Oscar", 15, "hombre", 6);
+		alumnos[4] = new Alumno("Ana", 14, "mujer", 7);
+		alumnos[5] = new Alumno("Jaume", 13, "hombre", 8);
+		alumnos[6] = new Alumno("Juan", 12, "hombre", 8);
+		alumnos[7] = new Alumno("Joel", 14, "hombre", 8);
+		alumnos[8] = new Alumno("Rosa", 15, "mujer", 7);
+		alumnos[9] = new Alumno("Marta", 13, "mujer", 8);
+		
+		return alumnos;
 	}
 }
